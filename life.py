8c2c5904825e08d2f3913cded7051699642be66a
Aiden Cullo <aidencullo@gmail.com>
2023-08-16 22:31:51 -0300

    back into a single board class

--- life.py
+++ life.py
@@ -5,35 +5,41 @@ pp = pprint.PrettyPrinter(indent=4)
 
 def row(size):
     return [0 for x in range(size)]
-
-def evolve(board):
-    for (x, y), element in np.ndenumerate(np.array(board.grid.value)):
-        evolve_cell(board.grid, x, y)
-
-def evolve_cell(grid, x, y):
-    print(grid.get(x + 1, y))
-    print(grid.get(x, y))
-    print(grid.get(x - 1, y))
           
-class Grid:    
-
-    def __init__(self, size):
-        self.value = [row(size) for x in range(size)]
-        self.value[2][2] = 1
-
-    def get(self, x, y):
-        return x in range(0, len(self.value))
-        
 class Board:
 
     def __init__(self, size):
-        self.grid = Grid(size)
+        self.grid = [row(size) for x in range(size)]
+        self.grid[0][0] = 1
 
     def print(self):
         pp.pprint(self.grid)
         print()
+
+    def get(self, x, y):
+        if x in range(0, len(self.grid)) and y in range(0,
+        len(self.grid[0])):
+            return self.grid[x][y]
+        else:
+            return 0
+
+    def evolve(self):
+        for (x, y), lement in np.ndenumerate(np.array(self.grid)):
+            self.evolve_cell(x, y)
+
+    def count_neighbors(self, x,y):
+        count = 0
+        self.get
+        return count
+
+    def evolve_cell(self, x, y):
+        if self.get(x, y):
+            print('active cell')
+            count = self.count_neighbors(x, y)
+            print(count)
+        else:
+            print('dead cell')
         
 board = Board(5)
-board.print()
-evolve(board)
-board.print()
+board.evolve()
+# board.evolve().print()
