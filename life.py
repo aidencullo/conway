0569ff27fd94727415684fb07c074d4e343f69ec
Aiden Cullo <aidencullo@gmail.com>
2023-08-21 20:32:38 -0300

    fixed double array copy issue -- still not working 100%

--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-#### Libraries
+### Libraries
 
 # standard
 import random
@@ -7,36 +7,50 @@ import time
 # third party
 import numpy as np
 
-size = 4
+size = 10
+steps = 2
+enabled = True
 
-def row(size):
+def row(size, num):
+    return [num for x in range(size)]
+
+def random_row(size):
     return [random.randint(0, 1) for x in range(size)]
 
-def repeating_row(size):
-    return [1 if y == size // 2 else 0 for y in range(size)]
-
 def random_grid(size):
-    return [row(size) for x in range(size)]
+    return [random_row(size) for x in range(size)]
 
 def repeating_grid(size):
+    def repeating_row(size):
+        return [1 if y == size // 2 else 0 for y in range(size)]
     return [repeating_row(size) for x in range(size)]
 
-grid = repeating_grid(size)
-# grid = random_grid(size)
+def zero_grid(size):
+    return np.zeros((size, size))
+
+# default from https://playgameoflife.com/
+def test_grid_1(size):
+    grid = zero_grid(size)
+    grid[5][5] = 1
+    grid[6][6] = 1
+    grid[6][7] = 1
+    grid[5][7] = 1
+    grid[4][7] = 1
+    return grid.transpose()
 
 def get(x, y):
     if x in range(0, len(grid)) and y in range(0, len(grid[0])):
-        # print('in range')
-        # print('x:{0}, y:{1}'.format(x,y))
         return grid[x][y]
     else:
         return 0
 
+def copy_grid(grid):
+     return [row[:] for row in grid]
+
 def evolve():
-    new_grid = [*grid]
+    new_grid = copy_grid(grid)
     for (y, x), element in np.ndenumerate(np.array(grid)):
         new_grid[x][y] = evolve_cell(x, y)
-        print(x,y, element)
     return new_grid
         
 def count_neighbors( x,y):
@@ -53,23 +67,26 @@ def count_neighbors( x,y):
 
 def evolve_cell( x, y):
     count = count_neighbors(x, y)
-    # print('count: {0} get(x,y): {1}, x:{2}, y:{3}'.format(count, get(x,y),x,y))
     if get(x, y):
         if count not in range(2, 4):
             return 0
     else:
         if count == 3:
             return 1
-    # print(grid[x][y])
     return grid[x][y]
 
 def print_grid():
     print(*grid, sep='\n')
     print()
 
-i = 0
-while(i < 1):
-    i += 1
-    print_grid()
-    grid = evolve()
-    time.sleep(1)
+# if main == main
+# failed
+grid = test_grid_1(size)
+    
+if enabled:
+    i = 0
+    while(i < steps):
+        i += 1
+        print_grid()
+        grid = evolve()
+        time.sleep(1)
