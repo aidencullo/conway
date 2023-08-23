6c8ab8cbd45c49a351adb7e0d710f807f5043bad
Aiden Cullo <aidencullo@gmail.com>
2023-08-23 09:29:59 -0300

    split code into two files

---
+++ grids.py
@@ -0,0 +1,56 @@
+# third party
+import numpy as np
+
+size = 10
+steps = 10
+
+def row(size, num):
+    return [num for x in range(size)]
+
+def random_row(size):
+    return [random.randint(0, 1) for x in range(size)]
+
+def random_grid(size):
+    return [random_row(size) for x in range(size)]
+
+def repeating_grid(size):
+    def repeating_row(size):
+        return [1 if y == size // 2 else 0 for y in range(size)]
+    return [repeating_row(size) for x in range(size)]
+
+def zero_grid(size):
+    return np.zeros((size, size))
+
+# default from https://playgameoflife.com/
+def test_grid_1(size):
+    grid = zero_grid(size)
+    grid[5][5] = 1
+    grid[6][6] = 1
+    grid[6][7] = 1
+    grid[5][7] = 1
+    grid[4][7] = 1
+    return grid.transpose()
+
+def beehive(size):
+    grid = zero_grid(size)
+    grid[5][5] = 1
+    grid[5][6] = 1
+    grid[6][7] = 1
+    grid[7][5] = 1
+    grid[7][6] = 1
+    grid[6][4] = 1
+    return grid
+
+def beacon(size):
+    grid = zero_grid(size)
+    grid[5][5] = 1
+    grid[5][6] = 1
+    grid[6][5] = 1
+
+    grid[7][8] = 1
+    grid[8][7] = 1
+    grid[8][8] = 1
+
+    return grid
+
+grid = beacon(size)
--- life.py
+++ life.py
@@ -4,61 +4,11 @@
 import random
 import time
 
-# third party
-import numpy as np
+# local modules
+from grids import grid
 
-size = 10
-steps = 10
-enabled = True
-
-def row(size, num):
-    return [num for x in range(size)]
-
-def random_row(size):
-    return [random.randint(0, 1) for x in range(size)]
-
-def random_grid(size):
-    return [random_row(size) for x in range(size)]
-
-def repeating_grid(size):
-    def repeating_row(size):
-        return [1 if y == size // 2 else 0 for y in range(size)]
-    return [repeating_row(size) for x in range(size)]
-
-def zero_grid(size):
-    return np.zeros((size, size))
-
-# default from https://playgameoflife.com/
-def test_grid_1(size):
-    grid = zero_grid(size)
-    grid[5][5] = 1
-    grid[6][6] = 1
-    grid[6][7] = 1
-    grid[5][7] = 1
-    grid[4][7] = 1
-    return grid.transpose()
-
-def beehive(size):
-    grid = zero_grid(size)
-    grid[5][5] = 1
-    grid[5][6] = 1
-    grid[6][7] = 1
-    grid[7][5] = 1
-    grid[7][6] = 1
-    grid[6][4] = 1
-    return grid
-
-def beacon(size):
-    grid = zero_grid(size)
-    grid[5][5] = 1
-    grid[5][6] = 1
-    grid[6][5] = 1
-
-    grid[7][8] = 1
-    grid[8][7] = 1
-    grid[8][8] = 1
-
-    return grid
+# global constants
+enabled = False
 
 def get(x, y):
     if x in range(0, len(grid)) and y in range(0, len(grid[0])):
@@ -66,12 +16,9 @@ def get(x, y):
     else:
         return 0
 
-def copy_grid(grid):
-     return grid.copy()
-
 def evolve():
-    new_grid = copy_grid(grid)
-    for (y, x), element in np.ndenumerate(np.array(grid)):
+    new_grid = grid.copy()
+    for (y, x) in np.ndenumerate(np.array(grid)):
         new_grid[x][y] = evolve_cell(x, y)
     return new_grid
 
@@ -89,12 +36,10 @@ def count_neighbors( x,y):
 
 def evolve_cell( x, y):
     count = count_neighbors(x, y)
-    if get(x, y):
-        if count not in range(2, 4):
-            return 0
-    else:
-        if count == 3:
-            return 1
+    if get(x, y) and count not in range(2, 4):
+        return 0
+    if not get(x, y) and count == 3:
+        return 1
     return grid[x][y]
 
 def print_grid():
@@ -107,10 +52,7 @@ def print_grid():
                 line += 'x' 
         print(line)
 
-grid = beacon(size)
-
 if __name__=="__main__":
-    print('hello world')
     if enabled:
         i = 0
         while(i < steps):
