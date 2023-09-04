93d183c3a0d821c2c212b5ab8068d1a9eda64337
Aiden Cullo <aidencullo@gmail.com>
2023-09-04 19:56:08 -0300

    add special getter and setter to Grid

--- tools/grid.py
+++ tools/grid.py
@@ -14,44 +14,44 @@ class Grid:
     def __init__(self, grid = None):
         if grid is None:
             grid = np.zeros((const.SIDE_LENGTH, const.SIDE_LENGTH))
-        self.grid = grid
+        self._grid = grid
 
-    def get(self, x, y):
+    def __setitem__(self, item, value):
+        self._grid[item] = value
+
+    def __getitem__(self, indices):
         try:
-            return self.grid[x][y]
+            return self._grid[indices]
         except IndexError:
             return 0
-
-    def set(self, x, y, value):
-        self.grid[x][y] = value
         
     def evolve(self):
         new_grid = Grid()
-        for (y, x), value in np.ndenumerate(self.grid):
-            new_grid.set(x, y, self.evolve_cell(x, y))
+        for (y, x), value in np.ndenumerate(self._grid):
+            new_grid[x, y] = self.evolve_cell(x, y)
         return new_grid
 
     def count_neighbors(self, x, y):
-        count = sum(self.get(x + i, y + k) for i, k in itertools.product(range(-1, 2), range(-1, 2)))
-        return count - self.get(x, y)
+        count = sum(self[x + i, y + k] for i, k in itertools.product(range(-1, 2), range(-1, 2)))
+        return count - self[x, y]
 
     def evolve_cell(self, x, y):
         count = self.count_neighbors(x, y)
-        if self.get(x, y) and count not in range(2, 4):
+        if self[x, y] and count not in range(2, 4):
             return 0
-        if not self.get(x, y) and count == 3:
+        if not self[x, y] and count == 3:
             return 1
-        return self.get(x, y)
+        return self[x, y]
 
     def transpose(self):
-        return Grid(self.grid.transpose())
+        return Grid(self._grid.transpose())
     
     def print(self):
-        print('__' * (len(self.grid[0]) + 2))
-        for y in self.grid:
+        print('__' * (len(self._grid[0]) + 2))
+        for y in self._grid:
             print('| ', end='')
             for x in y:
                 print('x ' if x else '_ ', end='')
             print(' |', end='')
             print()
-        print('__' * (len(self.grid[0]) + 2))
+        print('__' * (len(self._grid[0]) + 2))
--- tools/sample.py
+++ tools/sample.py
@@ -6,31 +6,31 @@ from .grid import Grid
 # default from https://playgameoflife.com/
 def test_grid_1(size):
     grid = Grid()
-    grid.set(5, 5, 1)
-    grid.set(6, 6, 1)
-    grid.set(6, 7, 1)
-    grid.set(5, 7, 1)
-    grid.set(4, 7, 1)
+    grid[5, 5] = 1
+    grid[6, 6] = 1
+    grid[6, 7] = 1
+    grid[5, 7] = 1
+    grid[4, 7] = 1
     return grid.transpose()
 
 def beehive(size):
     grid = Grid()
-    grid.set(5, 5, 1)
-    grid.set(5, 6, 1)
-    grid.set(6, 7, 1)
-    grid.set(7, 5, 1)
-    grid.set(7, 6, 1)
-    grid.set(6, 4, 1)
+    grid[5, 5] = 1
+    grid[5, 6] = 1
+    grid[6, 7] = 1
+    grid[7, 5] = 1
+    grid[7, 6] = 1
+    grid[6, 4] = 1
     return grid
 
 def beacon(size):
     grid = Grid()
-    grid.set(5, 5, 1)
-    grid.set(5, 6, 1)
-    grid.set(6, 5, 1)
-    grid.set(7, 8, 1)
-    grid.set(8, 7, 1)
-    grid.set(8, 8, 1)
+    grid[5, 5] = 1
+    grid[5, 6] = 1
+    grid[6, 5] = 1
+    grid[7, 8] = 1
+    grid[8, 7] = 1
+    grid[8, 8] = 1
     return grid
 
 def seed_grid(size):
